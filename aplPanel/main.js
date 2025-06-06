fa11df3931004310feb637d2b45727e12685ddaf
ApliNi <NM70727@qq.com>
2025-06-06 11:00:17 +0800

    更新注释

--- aplPanel/main.js
+++ aplPanel/main.js
@@ -103,7 +103,7 @@ const scrollingUpdateStatsData = (sd) => {
 		sd.heatmap.push(...Array.from({ length: Math.min(dayDiff, 365) }, () => ([0, 0])));
 		sd.date.day += yearDiff;
 
-		// 减小常见用户代理和网络类型计数器的数值, 使近期新的数据能够更快反映在图表上
+		// 等比例缩小数据, 使新数据更快反映在图表上
 		if(true){
 			sd.all.network.v4 *= 0.85;
 			sd.all.network.v6 *= 0.85;
